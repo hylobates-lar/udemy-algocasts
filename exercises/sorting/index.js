d8a9e17cf7143419e8b926ab800ee16714c40d99
hylobates-lar <alison.v.quaglia@gmail.com>
2020-06-02 11:14:54 -0400

    merge and selection sort passing

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,54 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+       for (let j = 0; j < (arr.length - i - 1); j++) {
+            if (arr[j] > arr[j + 1]) {
+                const lesser = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = lesser;
+            }
+       }
+    }
+    return arr;
 }
 
 function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+       let indexOfMin = i;
 
+       for (let j = i+1; j < arr.length; j++) {
+           if (arr[j] < arr[indexOfMin]) {
+               indexOfMin = j;
+           }
+       }
+       
+       if (indexOfMin !== i) {
+           let lesser = arr[indexOfMin];
+           arr[indexOfMin] = arr[i];
+           arr[i] = lesser;
+       }
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
-
+    
 }
 
 function merge(left, right) {
+    // left and right are already sorted arrays
+    const results = [];
 
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift());
+        } else {
+            results.push(right.shift());
+        }
+    }
+
+    return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
